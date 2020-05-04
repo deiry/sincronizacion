28b5d18b17376f04d9ade8d40a6d9b3779f7eb0e
estefa942 <estefany.muriel@udea.edu.co>
2020-05-04 17:51:20 -0500

    added concurred list implementation

---
+++ Aplicación/concurrent_linked_list.c
@@ -0,0 +1,61 @@
+ ￼
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "concurrent_linked_list.h"
+
+void list_init(list_t *l) {
+    l->head = NULL;
+    pthread_mutex_init(&l->lock, NULL);
+}
+
+void list_insert(list_t *l, int key, book *b) {
+    // synchronization not needed
+    node_t *new = malloc(sizeof(node_t));
+    if (new == NULL) {
+        perror("malloc");
+        return;
+    }
+    new->key = key;
+    new->book = b;
+    pthread_mutex_lock(&L->lock); //I don't know
+    new->next = l->head;
+    l->head = new;
+    pthread_mutex_unlock(&L->lock);
+    new->available = 1;
+ }
+
+ int list_lookup(list_t *l, int key) {
+    int rv = -1;
+    pthread_mutex_lock(&l->lock);    
+    node_t *curr = l->head;
+    while (curr) {
+        if (curr->key == key) {
+            if(curr->available == 1){
+                curr->available = 0;
+                rv = 1;
+            }else{
+                rv = 0;
+            }
+            break;
+        }
+        curr = curr->next;
+    }
+    pthread_mutex_unlock(&l->lock);
+    return rv;
+}
+
+void books_init(list_t *l){
+    printf("Initializing list of available books\n");
+    for (int i = 0; i < 10000; i++)
+    {
+        char name_construction[10];
+        book *b = malloc(sizeof(book));
+        sprintf(name_construction, "book_%d", i);
+        b->id = i;
+        b->name = name_construction;
+        b->num_pages = 100;
+        b->pub_year = 2020;
+        list_insert(l,i,b);
+    }
+}
---
+++ Aplicación/concurrent_linked_list.h
@@ -0,0 +1,31 @@
+#ifndef CONCURRENT_LINKED_LIST_H
+#define CONCURRENT_LINKED_LIST_H
+#include <stdio.h>
+#include <pthread.h>
+
+typedef struct __book {
+ int id;
+ char* name;
+ int num_pages;
+ int pub_year;
+} book;
+
+typedef struct __node_t {
+ int key;
+ int available;
+ struct __book *book;
+ struct __node_t *next;
+} node_t;
+
+typedef struct __list_t {
+ node_t *head;
+ pthread_mutex_t lock;
+} list_t;
+
+void list_init(list_t *l);
+void insert_list(list_t *l, int key, book *b);
+int list_lookup(list_t *l, int key);
+void books_init(list_t *l);
+#endif
+
+
